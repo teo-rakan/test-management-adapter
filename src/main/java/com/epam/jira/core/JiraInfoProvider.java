6e231033141057f21647e6f4a940b53b17703310
Alena Zubrevich <Alena_Zubrevich@epam.com>
2017-11-03 14:30:56 +0300

    add Jira Info Provider for string values and files storing on Jira

---
+++ src/main/java/com/epam/jira/core/JiraInfoProvider.java
@@ -0,0 +1,112 @@
+package com.epam.jira.core;
+
+import com.epam.jira.JIRATestKey;
+import com.epam.jira.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class JiraInfoProvider {
+
+    private static Map<String, List<Parameter>> jiraKeyParametersMapping = new HashMap<>();
+    private static Map<String, List<String>> jiraKeyAttachmentsMapping = new HashMap<>();
+    private final static int MIN_CALL_DEPTH = 4;
+
+    private static String getCallerMethodJiraKey(int depth) {
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        return getCallerMethodJiraKey(trace, depth);
+    }
+
+    private static String getCallerMethodJiraKey(StackTraceElement[] trace, int depth) {
+        if (trace.length <= depth) return null;
+
+        String className = trace[depth].getClassName();
+        String methodName = trace[depth].getMethodName();
+
+        if (methodName.equals("invoke0")) return null;
+
+        try {
+            Class clazz = Class.forName(className);
+            Method stackMethod = null;
+
+            for (Method method : clazz.getMethods()) {
+                if (method.getName().equals(methodName)) {
+                    if (stackMethod != null)
+                        throw new RuntimeException("Cannot resolve method by name (multiple methods were found): " + methodName);
+                    stackMethod = method;
+                }
+            }
+            JIRATestKey jiraKey = stackMethod.getAnnotation(JIRATestKey.class);
+            if (jiraKey != null) return jiraKey.key();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot find class " + className + " from StackTrace element: " + trace[depth]);
+        }
+        return getCallerMethodJiraKey(trace, depth + 1);
+    }
+
+    private static String getCallerMethodJiraKey() {
+        return getCallerMethodJiraKey(MIN_CALL_DEPTH);
+    }
+
+    public static String saveFile(File file) {
+        String key = getCallerMethodJiraKey();
+
+        if (key == null) return null;
+        if (!file.exists() || !file.isFile()) return null;
+
+        System.out.println("FILE NOT NULL");
+        String currentDir = System.getProperty("user.dir");
+        String targetFilePath = null;
+
+        // Get relative path or copy to target
+        try {
+            String filePath = file.getCanonicalPath();
+            if (!filePath.startsWith(currentDir + FileUtils.getTargetDir())) {
+                targetFilePath = FileUtils.getTargetDir() + file.getName();
+                File copy = new File("." + targetFilePath);
+                org.apache.commons.io.FileUtils.copyFile(file, copy);
+            } else {
+                targetFilePath = filePath.replaceFirst(currentDir, "");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Map file path
+        if (jiraKeyAttachmentsMapping.containsKey(key)) {
+            jiraKeyAttachmentsMapping.get(key).add(targetFilePath);
+        } else {
+            List<String> files = new ArrayList<>();
+            files.add(targetFilePath);
+            jiraKeyAttachmentsMapping.put(key, files);
+        }
+
+        return targetFilePath;
+    }
+
+    public static void saveValue(String title, String value) {
+        String key = getCallerMethodJiraKey();
+
+        if (key != null) {
+            Parameter parameter = new Parameter(title, value);
+            if (jiraKeyParametersMapping.containsKey(key)) {
+                jiraKeyParametersMapping.get(key).add(parameter);
+            } else {
+                List<Parameter> params = new ArrayList<>();
+                params.add(parameter);
+                jiraKeyParametersMapping.put(key, params);
+            }
+        }
+    }
+
+    public static List<String> getIssueAttachments(String key) {
+        return jiraKeyParametersMapping.containsKey(key) ? jiraKeyAttachmentsMapping.get(key) : null;
+    }
+
+    public static List<Parameter> getIssueParameters(String key) {
+        return jiraKeyParametersMapping.containsKey(key) ? jiraKeyParametersMapping.get(key) : null;
+    }
+
+}
